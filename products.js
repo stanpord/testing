a9a53e58f56987d89c1835068beb37a9e3c33efe
Joshua stanaland <stanpord24@gmail.com>
2025-12-02 21:24:53 -0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,5 @@
+const products = [
+  { name:"Product A", price: 25.00, img:"imageA.jpg", desc:"Description A" },
+  { name:"Product B", price: 35.00, img:"imageB.jpg", desc:"Description B" },
+  // Add up to 20 or more products
+];
